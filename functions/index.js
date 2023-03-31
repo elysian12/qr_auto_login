e72e8e61d9d9b2367bb8d58869f8f3bc69cd1df3
elysian12 <elysian1232@gmail.com>
2023-03-31 11:17:23 +0530

    added firebase function to create token

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,20 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const serviceAccount = require("./adminsdk.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+exports.getToken = functions.https.onCall(async (data, context) => {
+  return admin
+      .auth()
+      .createCustomToken(data.uid)
+      .then((customToken) => {
+        return customToken;
+      })
+      .catch((error) => {
+        return null;
+      });
+});
